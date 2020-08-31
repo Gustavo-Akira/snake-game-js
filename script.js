28a5055c2b0b52049ed4da7ca4e70c3457d124a5
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2020-08-31 16:14:53 -0300

    pausing on the game over

--- script.js
+++ script.js
@@ -44,13 +44,13 @@ function drawFood(){
     context.fillRect(food.x, food.y, box, box);
 }
 function startGame(){
-    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-    if(snake[0].x < 0 && direction == "left") snake[0].x = 16*box;
-    if(snake[0].y > 15 * box && direction == "up") snake[0].y = 0;
-    if(snake[0].y < 0 && direction == "down") snake[0].y = 16 * box;
+    if(snake[0].x > 15 * box) snake[0].x = 0;
+    if(snake[0].x < 0) snake[0].x = 16*box;
+    if(snake[0].y > 15 * box ) snake[0].y = 0;
+    if(snake[0].y < 0) snake[0].y = 16 * box;
     for(i = 1; i< snake.length; i++){
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
-            clearInterval();
+            clearInterval(jogo);
             alert('Game Over :(');
         }
     }
