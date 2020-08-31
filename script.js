d618a99656e7065851382169ee4e9a414555da3c
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2020-08-31 15:45:37 -0300

    adding  actions and preventing of out of the border

--- script.js
+++ script.js
@@ -11,7 +11,23 @@ function createBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0,0,16*box, 16*box);
 }
-
+document.addEventListener('keydown',update);
+function update(event){
+    switch(event.keyCode){
+        case 37:
+            direction ="left";
+        break;
+        case 38:
+            direction ="down";
+        break;
+        case 39:
+            direction ="right";
+        break;
+        case 40:
+            direction ="up";
+        break;
+    }
+}
 function createSnake(){
     for(i=0;i<snake.length;i++){
         context.fillStyle = "green";
@@ -19,6 +35,10 @@ function createSnake(){
     }
 }
 function startGame(){
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16*box;
+    if(snake[0].y > 15 * box && direction == "up") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "down") snake[0].y = 16 * box;
     createBG();
     createSnake();
     let snakeX = snake[0].x;
